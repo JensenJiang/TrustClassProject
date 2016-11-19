b1c1d2f59f47f9cb0468f968fdc1211b1a0a40e1
Jensen Jiang <jensejiang1995@gmail.com>
2016-11-19 23:32:57 +0800

    update serializers

--- TrustClassProject/databaseOperation/serializers.py
+++ TrustClassProject/databaseOperation/serializers.py
@@ -9,25 +9,41 @@ class UserProfileSerializer(serializers.ModelSerializer):
         extra_kwargs = {'username': {'read_only': True}}
 
     def update(self, instance, validated_data):
-        instace.email = validated_data.get('email', instance.email)
+        instance.email = validated_data.get('email', instance.email)
         instance.save()
         return instance
 
 class SchoolSerializer(serializers.ModelSerializer):
     class Meta:
         model = School
-        fields = ('name',)
+        fields = ('name', 'pk')
+
+class SchoolUpdateSerializer(serializers.Serializer):
+    pk = serializers.IntegerField()
 
 class TCUserProfileSerializer(serializers.ModelSerializer):
     user = UserProfileSerializer()
     school = SchoolSerializer()
+    class Meta:
+        model = TCUser
+        fields = ('nickname', 'school', 'user')
+ 
+class TCUserProfileUpdateSerializer(serializers.ModelSerializer):
+    user = UserProfileSerializer()
+    school = SchoolUpdateSerializer()
 
     class Meta:
         model = TCUser
         fields = ('nickname', 'school', 'user')
     
     def update(self, instance, validated_data):
-        print(validated_data)
+        self.get_fields()['user'].update(instance.user, validated_data.get('user'))
+        new_pk = validated_data.get('school').get('pk', instance.school.pk)
+        if new_pk != instance.school.pk:
+            try:
+                instance.school = School.objects.get(pk=new_pk)
+            except School.DoesNotExist:
+                pass
         instance.nickname = validated_data.get('nickname', instance.nickname)
         instance.save()
         return instance
